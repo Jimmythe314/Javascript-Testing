b585da17b8a40523e77e80151c0918bb2704aa1f
Jimmythe314 <Jimmythe314@gmail.com>
2015-12-06 21:29:01 +1100

    Update Javascript_Testing.js

--- Javascript_Testing.js
+++ Javascript_Testing.js
@@ -1,18 +1,1 @@
-# Javascript-Testing
-<DOCKTYPE HTML>
-    <html>
-        <head>
-            <title>Hello it's me</title>
-        </head>
-        <body>
-              <script>
-              var family = ("Australia, NSW, Cherrybrook");
-              if (family == "DOGE DOGE DOGE DOGE") {
-                document.write ("Congratulation you somehow won");
-              }
-              else {
-                document.write ("Failed attempt lol");
-              }
-             </script>
-        </body>
-    </html>
+
